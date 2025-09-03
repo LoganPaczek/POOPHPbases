2ed671e2f5c01058bca71626fdba704cbd7040f3
LoganPaczek <paczek.logan05@gmail.com>
2025-09-03 16:54:00 +0200

    feat: make we can create a arme class

--- mesClasses/Arme.php
+++ mesClasses/Arme.php
@@ -1,7 +1,18 @@
 <?php
 
 class Arme{
-        
+        // Attributs
+        private string $name;
+        private string $image;
+        private string $description;
+
+
+        // Constructeur
+        public function __construct(string $nom, string $image, string $description){
+            $this->nom = $nom;
+            $this->image = $image;
+            $this->description = $description;
+        }
     }
 
 
